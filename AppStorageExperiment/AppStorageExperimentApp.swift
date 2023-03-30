9d1de07e6d413c39566835fcabcd021f5b06b1c8
Paul Calnan <pwc3@users.noreply.github.com>
2023-03-30 10:10:24 -0400

    Initial Commit

---
+++ AppStorageExperiment/AppStorageExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageExperimentApp.swift
+//  AppStorageExperiment
+//
+//  Created by Paul Calnan on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
